8bdc2627ed4f1d3a1c7d2d6597fbfde0a71f79da
Eric de Ruiter <eric@thisisdevelopment.nl>
2023-06-21 16:49:51 +0200

    Updated to laravel 10

--- app/App/Models/User.php
+++ app/App/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,5 +42,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 }
